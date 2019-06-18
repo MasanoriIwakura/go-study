001d9a3aa8994f35b53c5af8dc05be083764c37a
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-06-18 16:19:42 +0900

    Add if ,switch and for examples

--- hello.go
+++ hello.go
@@ -29,4 +29,55 @@ func main() {
 	fmt.Printf("str=%s\n", str)
 	fmt.Printf("b1=%t, b2=%t\n", b1, b2)
 	fmt.Printf("f1=%f, f2=%f\n", f1, f2)
+
+	fmt.Println("---------------------")
+
+	branch()
+
+	fmt.Println("---------------------")
+
+	loop()
+}
+
+// 分岐構文
+func branch() {
+	b1 := 0
+	b2 := 3
+
+	// if sample
+	if b1 > 1 {
+		// noop.
+	} else if b1 == 1 {
+		// noop.
+	} else {
+		fmt.Println("b1 is Zero")
+	}
+
+	// switch sample
+	switch b2 {
+	case 1:
+		fmt.Println("b2 is One")
+	case 2:
+		fmt.Println("b2 is Two")
+	case 3:
+		fmt.Println("b2 is Three")
+	}
+}
+
+// ループ構文例
+func loop() {
+	// for sample
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d ", i)
+	}
+
+	fmt.Println()
+
+	// foreach sample
+	r := []int{2, 4, 8, 16, 32, 64, 128, 256, 512}
+	for i, j := range r {
+		fmt.Printf("%d:%d ", i, j)
+	}
+
+	fmt.Println()
 }
