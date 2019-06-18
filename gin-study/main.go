e4d223a8f8085d7b884771b889c0f9cc4fe6c23f
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-06-19 01:07:05 +0900

    Update gin-study
    Add css and pages

--- gin-study/main.go
+++ gin-study/main.go
@@ -7,9 +7,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Link struct {
+	Name string
+	Url  string
+}
+
+var links = []Link{
+	Link{Name: "About", Url: "/about"},
+}
+
+const (
+	base = "templates/layouts/base.html"
+)
+
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.html")
+	r.Static("/css", "templates/css")
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -18,11 +32,18 @@ func main() {
 	})
 
 	r.GET("/", func(c *gin.Context) {
-		html := template.Must(template.ParseFiles("templates/layouts/base.html", "templates/index.html"))
+		html := template.Must(template.ParseFiles(base, "templates/index.html"))
 		r.SetHTMLTemplate(html)
 		c.HTML(http.StatusOK, "base.html", gin.H{
 			"message": "Template Sample",
+			"links":   links,
 		})
+	})
+
+	r.GET("/about", func(c *gin.Context) {
+		html := template.Must(template.ParseFiles(base, "templates/pages/about.html"))
+		r.SetHTMLTemplate(html)
+		c.HTML(http.StatusOK, "base.html", gin.H{})
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080
